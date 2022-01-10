d1b7fb3b2bc2f563ebe2011e581ce79d21752235
thasup <thanachonfirst@hotmail.com>
2022-01-10 21:53:57 +0700

    Add noteController

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,31 @@
+import asyncHandler from "express-async-handler";
+import Note from "../models/noteModel.js";
+
+// @desc    Create note
+// @route    POST /api/notes/
+// @access      Public
+export const createNote = asyncHandler(async (req, res) => {
+    const { title, content } = req.body;
+
+    const noteExist = await Note.fineOne({ title, content });
+
+    if (noteExist) {
+        res.status(400);
+        throw new Error("Note aresdy exists");
+    }
+
+    const newNote = await Note.create({
+        title,
+        content,
+    });
+
+    if (newNote) {
+        res.status(201).json({
+            noteTitle: newNote.title,
+            noteContent: newNote.content,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid note data");
+    }
+});
