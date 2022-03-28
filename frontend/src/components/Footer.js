c853366cf104d9d5da9cf5600db925216e1cb8c9
thasup <thanachonfirst@hotmail.com>
2022-03-28 14:29:57 +0700

    Edit: footer link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,18 @@
 import React from "react";
 
 function Footer() {
-    const localDate = new Date();
-    const currentYear = localDate.getFullYear();
-    return (
-        <footer>
-            <p>Copyright &copy; thasup {currentYear}</p>
-        </footer>
-    );
+  const localDate = new Date();
+  const currentYear = localDate.getFullYear();
+  return (
+    <footer>
+      <p>
+        <span>&copy; Copyright {currentYear} </span>{" "}
+        <a className="footer-link" target="blank" href="https://thanachon.me">
+          THASUP
+        </a>
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
